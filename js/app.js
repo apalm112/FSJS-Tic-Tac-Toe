a64c5e9878b4ab064c5213759818a1a1fa76b066
apalm112 <apalm112@gmail.com>
2017-03-13 15:19:03 -0700

    Added the initial win screen for player1 & started a count function to toggle turns between players.

--- js/app.js
+++ js/app.js
@@ -7,13 +7,17 @@
   const $start = $('#start');
   // Get the button that hides the start screen
   const $button = $('.button');
+  const $finish1 = $('#finish1');
+  const $finish2 = $('#finish2');
+  const $tie = $('#tie');
+
   const $player1 = $('#player1');
   const $player2 = $('#player2');
   const $board = $('#board');
   const $boxes = $('.boxes');
   const twoDimArr = [];
   const game = document.querySelectorAll('.boxes');
-
+  let count = 0;
 
   const player1 = {
     name: 'player1',
@@ -57,13 +61,31 @@
       function() {
         $(this).css({'background-image': 'url(' + player.svg + ')'});
         $(this).css({'background-color': player.bgColor});
+        count++;
+        console.log(count);
+        increaseCount();
       }
     )
   }
 
 
+  $finish1.css('display', 'none');
+  $finish2.css('display', 'none');
+  $tie.css('display', 'none');
 
 
+  function increaseCount() {
+    if (count < 9) {
+      togglePlayer();
+    } else if (count > 9) {
+      $finish1.css('display', 'block');
+    }
+  }
+
+  function togglePlayer() {
+    $player1.toggleClass('active');
+    $player2.toggleClass('active');
+  }
 
 
 }) (window);
