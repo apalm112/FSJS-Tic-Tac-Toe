ca3052fda7f9be602ec7937adce546531bca1121
sofa_king <apalm112@gmail.com>
2017-07-14 19:05:59 -0700

    mostly working, might be a jquery issue

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+/* eslint-disable*/
+// ABOVE LINE DISABLES ESLINT FOR THIS FILE.
 // Treehouse Project-04: Tic-Tac-Toe Game
 'use strict';
 (function(module) {
@@ -56,7 +58,7 @@
   let gridSquare = document.querySelectorAll('.box');
 
 
-
+  let turn = 0;
   // Count something????
   let count = 0;
 
@@ -132,11 +134,10 @@
     $start.css('display', 'none');
     $finish1.css('display', 'none');
     $finish2.css('display', 'none');
-      setGridToZero('hobbits');
+      setGridToZero('MT');
       $player1.toggleClass('active');
       hover(player1);
       clickIt(player1);
-    //  togglePlayer();
     });
   }
 
@@ -153,100 +154,80 @@
   function hover(player) {
   // Meets rubric: When the current player mouses over an empty square on the board, it's symbol the X or O should appear on the square. Also does color, NOT NEEDED.
   // Function checks if grid square is empty, if so then displays current player symbol & color on grid.
-    $('.box').hover(function() {
-      if ($(this)[0].attributes[1].value === 'hobbits') {
-        $(this).toggleClass(player.boxFill);
-      }
-    });
+    $('.box').hover(
+      function() {
+      // TODO:  what if, write a Conditional that checks the turn number and then shows the corresponding player boxFill
+        // if (turn === 0 || turn === 2 || turn === 4 || turn === 6 || turn === 8) {
+        //   if ($(this)[0].attributes[1].value === 'MT') {
+        //     $(this).toggleClass('box-filled-1');
+            $(this).toggleClass(player.boxFill, 'MT');
+            // console.log( $(this)[0]);
+            // console.log(player.boxFill);
+          // } else if (turn === 1 || turn === 3 || turn === 5 || turn === 7) {
+        //    $(this).toggleClass('box-filled-2');
+          });
+        // }
+      // });
   }
-
 
   function clickIt(player) {
     // TODO: function is not dropping player1 class box-filled-1, won't add a 'X' to the game board.
     // Slack from N Hampton: You would assign another .one() handler to each of those boxes in that instance. `jQuery.one()` is the same as `jQuery.on()`, except that it runs `$(this).off(event)` at the end of the callback
-    // $('.box').click(
-    //   function() {
       $('.box').one('click', function() {
-        if ($(this)[0].attributes[1].value === 'hobbits') { // This line works, checked in console!
+        if ($(this)[0].attributes[1].value === 'MT') { // This line works, checked in console!
         // Conditional checks the board li to see if it's MT, if MT then change color/img.
-          // console.log('$(this) is on line below: ');
-          // console.log( $(this)[0].attributes[1].value );
-          // console.log( $(this) );
-
-          // turnController($(this), player);
-
-
           $(this).css({'background-image': 'url(' + player.svg + ')'});
           $(this).css({'background-color': player.bgColor});
-      // Set atr to 'O' or 'X'
+          // Set atr to 'O' or 'X'
           $(this)[0].setAttribute('fuck', player.grid);
+          // console.log('$(this)[0].attribute[1].value is on line below: ');
+          // console.log( $(this)[0].attributes[1].value );
+          // console.log( $(this).attr('fuck', player.grid) );
+          count++;
+          turn++;
+          console.log(turn);
+        //  hover();
+          increaseCount();
+          // console.log('player.grid is: ' + player.grid);
+          //  console.log(count + ' <--count, FUCK THIS SHIT!');
         }
-        count++;
-        //  increaseCount();
-
-      //  console.log('Player is: ' + player.grid);
-      //  console.log(count + ' <--count, FUCK THIS SHIT!');
       });
   }
 
-
-
-  function turnController(that, player) {
-    // ?Function to check & set atr value of grid square to 'O' or 'X'?
-    // And to control the flow of the game, ie.-- switching turns for players
-    that.css({'background-image': 'url(' + player.svg + ')'});
-    that.css({'background-color': player.bgColor});
-
-    // Set atr to 'O' or 'X'
-    that[0].setAttribute('fuck', player.grid);
-
-    console.log('that is on line below: ');
-    console.log( that[0].attributes[1].value );
-    console.log( that );
-
-    console.log('Player is: ' + player.grid);
-    console.log(count + ' <--count, FUCK THIS SHIT!');
-  }
-
   function increaseCount() {
-    // NOT WORKING
-    if ( $player1.hasClass('active')) {
-    // togglePlayer();
+    // Play alternates between X and O.
+    // NOT WORKING TODO:  I think this function is where the problem is.  As hover() & click() functions both work.
+    // It's only when this function is called that shit starts fucking up.
+    $player1.toggleClass('active');
+    $player2.toggleClass('active');
+    hover(player2);
+
+
+
+
+
+
+
+
+    // This block below is shit.
+  /*  if ( $player1.hasClass('active') && count < 9) {
       $player1.toggleClass('active');
       $player2.toggleClass('active');
       hover(player2);
-      //clickIt(player2);
-    } else if ($player2.hasClass('active')) {
+      // clickIt(player2);
+    } else if ($player2.hasClass('active') && count < 9) {
       $player2.toggleClass('active');
       $player1.toggleClass('active');
       hover(player1);
-      //clickIt(player1);
+      // clickIt(player1);
     } else if (count >= 9) {
       $finish1.css('display', 'block');
       count = 0;
       startGame();
-    }
-  }
-
-  function togglePlayer() {
-    // Changes the active O or X on game board.
-    // TODO: fix toggle switching btwn players!
-    if (count === 0) {  // Only runs at start of new game to turn on player1
-      $player1.toggleClass('active'); // turn on player1
-      hover(player1);
-    } else if (count > 0 && count < 2) {
-      $player1.toggleClass('active');
-      $player2.toggleClass('active');
-      hover(player2);
-    } else if (count >= 2) {
-      $player1.toggleClass('active');
-      $player2.toggleClass('active');
-      hover(player1);
-    }
+    } */
   }
 
   // Add the game play following these rules:
-  // Play alternates between X and O.
   // The current player is indicated at the top of the page -- the box with the symbol O or X is highlighted for the current player. You can do this by simply adding the class .active to the proper list item in the HTML. For example, if it's player one's turn, the HTML should look like this: <li class="players active" id="player1">
   // When the current player mouses over an empty square on the board, it's symbol the X or O should appear on the square. You can do this using the x.svg or o.svg graphics (hint use JavaScript to set the background-image property for that box.)
   // Players can only click on empty squares. When the player clicks on an empty square, attach the class box-filled-1 (for O) or box-filled-2 (for X) to the square. The CSS we're providing will automatically add the proper image to the square marking it as occupied.
