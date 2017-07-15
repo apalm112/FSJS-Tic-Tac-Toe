17db779b7665f43ce08bafd83e9fc38f850fd977
sofa_king <apalm112@gmail.com>
2017-07-14 22:35:05 -0700

    stuff

--- js/app.js
+++ js/app.js
@@ -51,19 +51,13 @@
     []
   ];
   // Get all the li's of the game grid
-  // EVEN BEING USED??????????
   const game = document.querySelectorAll('.boxes');
-
   // Trying to code 7/13/17
   let gridSquare = document.querySelectorAll('.box');
-
-
   let turn = 0;
   // Count something????
   let count = 0;
 
-
-  // Are these needed???? Prolly...
   const player1 = {
     name: 'player1',
     bgColor: '#FFA000',
@@ -71,27 +65,21 @@
     boxFill: 'box-filled-1',
     isWinner: false,
     grid: 'O',
-    specialBox: 'box box-filled-1'
   };
-
   const player2 = {
     name: 'player2',
     bgColor: '#3688C3',
     svg: 'img/x.svg',
     boxFill: 'box-filled-2',
     isWinner: false,
-    grid: 'X'
+    grid: 'X',
   };
 
   let liNodeList = document.querySelectorAll('.box');
-
   let liHTMLCollection = document.getElementsByClassName('.box');
 
   const boardState = {
     // Object to experiment w/ making an Object to track the state of the game in the DOM & maybe help control the game w/ its own methods.
-
-
-
   };
 
 // Main Functions ------------------------------------------------------------
@@ -132,9 +120,9 @@
     // On button click, start the game.
     $button.on('click', function() {
     //  enterName();
-    $start.css('display', 'none');
-    $finish1.css('display', 'none');
-    $finish2.css('display', 'none');
+	    $start.css('display', 'none');
+	    $finish1.css('display', 'none');
+	    $finish2.css('display', 'none');
       setGridToZero('MT');
       $player1.toggleClass('active');
       hover(player1);
@@ -148,53 +136,33 @@
 
   function setGridToZero(num) {
     for (let idx=0;idx<liZero.length;idx++) {
-     liZero[idx].setAttribute('fuck', num);
+     liZero[idx].setAttribute('gridValue', num);
     }
   }
 
   function hover(player) {
-  // Meets rubric: When the current player mouses over an empty square on the board, it's symbol the X or O should appear on the square. Also does color, NOT NEEDED.
   // Function checks if grid square is empty, if so then displays current player symbol & color on grid.
     $('.box').hover(
       function() {
-      // TODO:  what if, write a Conditional that checks the turn number and then shows the corresponding player boxFill
-        // if (turn === 0 || turn === 2 || turn === 4 || turn === 6 || turn === 8) {
-        //   if ($(this)[0].attributes[1].value === 'MT') {
-        //     $(this).toggleClass('box-filled-1');
             $(this).toggleClass(player.boxFill, 'MT');
-            // console.log( $(this)[0]);
-            // console.log(player.boxFill);
-          // } else if (turn === 1 || turn === 3 || turn === 5 || turn === 7) {
-        //    $(this).toggleClass('box-filled-2');
           });
-        // }
-      // });
   }
-
   function clickIt(player) {
     // TODO: function is not dropping player1 class box-filled-1, won't add a 'X' to the game board.
     // Slack from N Hampton: You would assign another .one() handler to each of those boxes in that instance. `jQuery.one()` is the same as `jQuery.on()`, except that it runs `$(this).off(event)` at the end of the callback
       $('.box').one('click', function() {
-        if ($(this)[0].attributes[1].value === 'MT') { // This line works, checked in console!
-        // Conditional checks the board li to see if it's MT, if MT then change color/img.
+				// Conditional checks the board li to see if it's MT, if MT then change color/img.
+        if ($(this)[0].attributes[1].value === 'MT') {
           $(this).css({'background-image': 'url(' + player.svg + ')'});
           $(this).css({'background-color': player.bgColor});
-          // Set atr to 'O' or 'X'
-          $(this)[0].setAttribute('fuck', player.grid);
-          $(this)[0].setAttribute('class', player.specialBox);
-
-          // console.log('$(this)[0].attribute[1].value is on line below: ');
-          // console.log( $(this)[0].attributes[1].value );
-          // console.log( $(this).attr('fuck', player.grid) );
-          count++;
-          turn++;
-          console.log(turn);
-        //  hover();
-          // increaseCount();
-          // console.log('player.grid is: ' + player.grid);
-          //  console.log(count + ' <--count, FUCK THIS SHIT!');
-        }
-      });
+					// Set value to 'O' or 'X'
+					$(this)[0].setAttribute('gridValue', player.grid);
+					count++;
+					turn++;
+					console.log('Turn is: ' + turn);
+					increaseCount();
+					}
+			});
   }
 
   function increaseCount() {
@@ -204,36 +172,7 @@
     $player1.toggleClass('active');
     $player2.toggleClass('active');
     hover(player2);
-
-
-
-
-
-
-
-
-    // This block below is shit.
-  /*  if ( $player1.hasClass('active') && count < 9) {
-      $player1.toggleClass('active');
-      $player2.toggleClass('active');
-      hover(player2);
-      // clickIt(player2);
-    } else if ($player2.hasClass('active') && count < 9) {
-      $player2.toggleClass('active');
-      $player1.toggleClass('active');
-      hover(player1);
-      // clickIt(player1);
-    } else if (count >= 9) {
-      $finish1.css('display', 'block');
-      count = 0;
-      startGame();
-    } */
+		// clickIt(player2);
   }
-
-  // Add the game play following these rules:
-  // The current player is indicated at the top of the page -- the box with the symbol O or X is highlighted for the current player. You can do this by simply adding the class .active to the proper list item in the HTML. For example, if it's player one's turn, the HTML should look like this: <li class="players active" id="player1">
-  // When the current player mouses over an empty square on the board, it's symbol the X or O should appear on the square. You can do this using the x.svg or o.svg graphics (hint use JavaScript to set the background-image property for that box.)
-  // Players can only click on empty squares. When the player clicks on an empty square, attach the class box-filled-1 (for O) or box-filled-2 (for X) to the square. The CSS we're providing will automatically add the proper image to the square marking it as occupied.
-  // The game ends when one player has three of their symbols in a row either horizontally, vertically or diagonally. If all of the squares are filled and no players have three in a row, the game is a tie.
 
 })(window);
